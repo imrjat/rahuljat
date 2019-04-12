3bd5314a79bd168f2b1759a4284edfb5a3ae63b5
Rahul Jat <imrjat@gmail.com>
2019-04-12 14:10:36 -0400

    dhdhhdd

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$currency = '₹';
+$db_username = 'rahul';
+$db_password = 'asdf1234';
+$db_name = 'on-shop';
+$db_host = 'localhost';
+$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
+?>
